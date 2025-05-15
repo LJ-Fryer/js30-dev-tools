a1cccc19ec86d6443425158012ad309a6efbc7f1
LJ-Fryer <ljf_aus@hotmail.com>
2025-05-15 15:43:11 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+console.log("Testing");
+
+const dogs = [
+  { name: "Snickers", age: 2 },
+  { name: "hugo", age: 8 },
+];
+
+function makeGreen() {
+  const p = document.querySelector("p");
+  p.style.color = "#eb4f42";
+  p.style.fontSize = "50px";
+}
+
+// Regular
+
+// Interpolated
+
+// Styled
+
+// warning!
+
+// Error :|
+
+// Info
+
+// Testing
+
+// clearing
+
+// Viewing DOM Elements
+
+// Grouping together
+
+// counting
+
+// timing
